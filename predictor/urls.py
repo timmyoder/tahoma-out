e6b7cccc19377a70f05ab7bdf7c35dbf69ad46df
Tim Yoder <tim.a.yoder@gmail.com>
2022-02-03 07:59:42 -0800

    About page

--- predictor/urls.py
+++ predictor/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from predictor.views import home, sources, stats, api, archive
+from predictor.views import home, about, stats, api, archive
 
 urlpatterns = [
     path("", home, name="home"),
-    path("sources/", sources, name="sources"),
+    path("about/", about, name="about"),
     path("stats/", stats, name="stats"),
     path("api/", api, name="api"),
     path("archive/", archive, name="archive"),
--- predictor/views.py
+++ predictor/views.py
@@ -7,7 +7,7 @@ import shutil
 from config import PHOTOS_DIR, MEDIA_DIR
 from training.retrieve_pics import get_current_image
 
-model = 'simple_cnn_weighted_small.h5'
+model = 'mobilenetV2_fine_tuned.h5'
 
 
 def render_random(request):
@@ -18,7 +18,7 @@ def render_random(request):
 
     shutil.copy(str(rand_choice), str(rand_pic))
 
-    predictor = Predictor(image=rand_pic, model_name=model)
+    predictor = Predictor(model_name=model, prediction_image=rand_pic)
     predictor.load_image()
     predictor.predict()
 
@@ -29,7 +29,7 @@ def render_random(request):
 def render_live(request):
     get_current_image()
     live_im = MEDIA_DIR / 'live.png'
-    predictor = Predictor(image=live_im, model_name=model)
+    predictor = Predictor(model_name=model, prediction_image=live_im)
     predictor.load_image()
     predictor.predict()
 
@@ -42,8 +42,8 @@ def home(request):
     # return render_random(request)
 
 
-def sources(request):
-    return render(request, 'sources.jinja2')
+def about(request):
+    return render(request, 'about.jinja2')
 
 
 def stats(request):
