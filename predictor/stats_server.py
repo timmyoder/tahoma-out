524cef84a6c2ecd8e5384200e42e8d80fe518c96
Tim Yoder <tim.a.yoder@gmail.com>
2022-03-14 18:58:20 -0700

    wrong filter

--- predictor/stats_server.py
+++ predictor/stats_server.py
@@ -3,5 +3,5 @@ from predictor.models import Photo
 
 class Stats:
     def __init__(self):
-        self.last_out = Photo.objects.filter(winner='Not out').latest('id')
+        self.last_out = Photo.objects.filter(winner='All the way out').latest('id')
 
