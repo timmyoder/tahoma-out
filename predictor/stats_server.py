797c2f7327f4ab59417fd35747700727c1b08054
Tim Yoder <tim.a.yoder@gmail.com>
2022-03-23 19:39:35 -0700

    pst time for stats

--- predictor/stats_server.py
+++ predictor/stats_server.py
@@ -1,7 +1,10 @@
 from predictor.models import Photo
+from pytz import timezone
 
 
 def date_formatter(date_to_format):
+    pst = timezone('US/Pacific')
+    date_to_format = date_to_format.astimezone(pst)
     date_time = date_to_format.strftime('%b. %-d, %Y, %-I:%M')
     am_pm = date_to_format.strftime('%p').lower()
     am_pm = f'{am_pm[0]}.{am_pm[1]}.'
