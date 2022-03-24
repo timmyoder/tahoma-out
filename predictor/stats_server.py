42c048095d9ed31ac0911a01fd4cd8b43bff4275
Tim Yoder <tim.a.yoder@gmail.com>
2022-03-23 19:18:13 -0700

    scrolling through out images

--- predictor/stats_server.py
+++ predictor/stats_server.py
@@ -1,7 +1,17 @@
 from predictor.models import Photo
 
 
+def date_formatter(date_to_format):
+    date_time = date_to_format.strftime('%b. %-d, %Y, %-I:%M')
+    am_pm = date_to_format.strftime('%p').lower()
+    am_pm = f'{am_pm[0]}.{am_pm[1]}.'
+    return f'Photo pulled {date_time} {am_pm}'
+
+
 class Stats:
     def __init__(self):
-        self.last_out = Photo.objects.filter(winner='All the way out').latest('id')
+        self.images = Photo.objects.filter(winner='All the way out').order_by('-id').all()
+        self.last_out = Photo.objects.filter(winner='All the way out').order_by('-id').first()
+        self.image_urls = [photo.image.url for photo in self.images]
+        self.labels = [date_formatter(photo.datetime) for photo in self.images]
 
